4c9ab74633416e0a848e474cf254e37285dca79a
abdelrahman anani <52931890+Abdelrahman-Anani@users.noreply.github.com>
2021-07-18 01:01:58 +0200

    Delete BIT_MATH.h

--- FOTA/include/BIT_MATH.h
+++
@@ -1,6 +0,0 @@
-#define SET_BIT(port,bit) (port |= (1<<bit))
-#define CLR_BIT(port,bit) (port &= (~(1<<bit)))
-#define TOGGLE_BIT(port,bit) (port ^= (1<<bit))
-
-#define GET_BIT(port,bit) (((port) >> (bit)) & 0x01)
-
